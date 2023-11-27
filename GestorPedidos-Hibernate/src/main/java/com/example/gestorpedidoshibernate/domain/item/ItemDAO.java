0ceb004ee6f37cf3ea597fb716c7ec28b10c2b3b
ElUltimoPasajero <116808960+ElUltimoPasajero@users.noreply.github.com>
2023-11-27 19:17:56 +0100

    Delete GestorPedidos-Hibernate directory

--- GestorPedidos-Hibernate/src/main/java/com/example/gestorpedidoshibernate/domain/item/ItemDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.gestorpedidoshibernate.domain.item;
-
-import java.util.List;
-
-public interface ItemDAO {
-
-    public List<Item> getAllItems();
-
-}
--- GestorPedidos-Hibernate/src/main/java/com/example/gestorpedidoshibernate/domain/order/OrderDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.gestorpedidoshibernate.domain.order;
-
-public interface OrderDAO {
-
-    public Order save(Order data);
-
-
-
-}
