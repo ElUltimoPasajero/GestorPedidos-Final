724215c4a8e06eb480f204d10056e3cfaa5e9ec2
ElUltimoPasajero <116808960+ElUltimoPasajero@users.noreply.github.com>
2024-01-27 13:02:16 +0100

    Delete DemostracionHilos_1 directory
    asd

--- DemostracionHilos_1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demostracionhilos_1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.demostracionhilos_1 to javafx.fxml;
-    exports com.example.demostracionhilos_1;
-}
